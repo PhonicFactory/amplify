66c6eadeb923ca715262f85f44261498b551a373
Roman, Greg <greg.roman@teamaol.com>
2017-12-26 11:09:25 -0500

    Fix broken profile page

--- src/components/profile.vue
+++ src/components/profile.vue
@@ -3,21 +3,23 @@
         <md-toolbar class="md-dense">
             <h2 class="md-title" style="flex: 1">Profile / Settings</h2>
         </md-toolbar>
-        <form>
-            <md-list>
-                <md-list-item>
-                    <md-avatar class="md-large">
-                        <img src="https://placeimg.com/64/64/people/1" alt="People">
-                    </md-avatar>
-                </md-list-item>
-                <md-list-item v-for="value, field in user" :key="field">
-                    <md-input-container>
-                        <label>{{ field }}</label>
-                        <md-input v-model="user[field]"></md-input>
-                    </md-input-container>
-                </md-list-item>
-            </md-list>
-        </form>
+        <md-layout>
+            <form>
+                <md-list>
+                    <md-list-item>
+                        <md-avatar class="md-large">
+                            <img src="https://placeimg.com/64/64/people/1" alt="People">
+                        </md-avatar>
+                    </md-list-item>
+                    <md-list-item v-for="value, field in user" :key="field">
+                        <md-input-container>
+                            <label>{{ field }}</label>
+                            <md-input v-model="user[field]"></md-input>
+                        </md-input-container>
+                    </md-list-item>
+                </md-list>
+            </form>
+        </md-layout>
     </md-layout>
 </template>
 <script>
