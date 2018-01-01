2e0f33498148770877e22dfac3167dc11cd63c7a
Roman, Greg <greg.roman@teamaol.com>
2017-12-31 21:58:53 -0500

    undo last commit

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const catchall = require('./catchall');
 const auth = require('./auth');
 
 module.exports = (app) => {
-    // app.use('/', catchall);
+    app.use('/', catchall);
     app.use('/auth', auth);
 };
