31738a1e345c4060cfbbbae1be99b80cc2e8c1d7
Brad Thorson <bbthorson@gmail.com>
2018-01-18 12:22:09 -0500

    Update sw-import.js

--- static/sw-import.js
+++ static/sw-import.js
@@ -2,8 +2,8 @@ self.addEventListener('push', function(event) {
     var title = 'ET Push Message';
     // Data must be encypted by server sending message
     // See this: https://developers.google.com/web/ilt/pwa/introduction-to-push-notifications#working_with_data_payloads
-    var data = event.data.json();
-    console.log(data);
+    //var data = event.data.json();
+    //console.log(data);
     console.log('Push message', arguments);
     event.waitUntil(
         self.registration.showNotification(title, {
