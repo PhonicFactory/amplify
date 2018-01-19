0369320c600fe4bc822fddd8c684e0769dec5b5e
Roman, Greg <greg.roman@teamaol.com>
2018-01-19 16:47:19 -0500

    add applicationserverkey

--- src/lib/push-notifications.js
+++ src/lib/push-notifications.js
@@ -1,13 +1,21 @@
 import store from '../store';
+import { urlB64ToUint8Array } from './helpers';
 
 function setSubscription(subscription) {
+    console.log(subscription);
     store.dispatch('setPushSubscription', subscription);
 }
 
 function subscribeNew() {
+
+    // https://github.com/GoogleChromeLabs/web-push-codelab/blob/master/app/scripts/main.js
+    // https://developers.google.com/web/updates/2016/03/web-push-encryption
     store.getters.pushManager
-        .subscribe({ userVisibleOnly: true })
-        .then(subscription => setSubscription(subscription))
+        .subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: urlB64ToUint8Array('BP4hOBy7KbKMhivtsyDyIBfrIQkD7BDCK0M8eiL9qbraBe-5rnT_-lRe_7REwhr5fMZwlhrSC9oQyyjjdmtuM6I')
+        })
+        .then(subscription => setSubscription(subscription.toJSON()))
         .catch((e) => {
             console.log('error getting push subscription')
         });
@@ -17,9 +25,8 @@ export function subscribe() {
     store.getters.pushManager
         .getSubscription()
         .then((subscription) => {
-            console.log(subscription);
             if (subscription) {
-                setSubscription(subscription);
+                setSubscription(subscription.toJSON());
                 return;
             }
             // Prompt user to subscribe for notifications
