ad165bd060293fed12ffdf274f462c893d981985
Brad Thorson <bbthorson@gmail.com>
2018-01-21 11:58:40 -0500

    removed index background removed bottom nav bar moved profile to left drawer, triggered by icon in upper left added right drawer for conversations did not remove call dialogs updated colors to blue, added light blue accent option updated some icons

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -16,10 +16,6 @@
             <md-layout md-flex class="page-container">
                 <component :is="tab" />
             </md-layout>
-            <md-bottom-bar md-theme="inverse">
-                <md-bottom-bar-item md-icon="forum" :md-active="tab === 'calls'" @click="$router.push({ name: 'calls' })">Conversations</md-bottom-bar-item>
-                <md-bottom-bar-item md-icon="account_circle" :md-active="tab === 'profile'" @click="$router.push({ name: 'profile' })">Profile</md-bottom-bar-item>
-            </md-bottom-bar>
         </md-layout>
     </md-layout>
 </template>
